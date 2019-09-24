def71017246c14f1546b395f75e6647e137248ea
Daniel Arturo Gonzalez Placito <daargonzalezpl@ittepic.edu.mx>
2019-09-24 07:39:31 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/ittepic/tpdm_u1_practica3_danielgonzalez/MainActivity.kt
@@ -0,0 +1,14 @@
+package mx.edu.ittepic.tpdm_u1_practica3_danielgonzalez
+
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
